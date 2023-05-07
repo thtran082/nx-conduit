6960093d77c0c572b1d2697a1f0fae9ed0f4fc9a
huytran082 <huy.tran@xomad.com>
2023-05-07 15:25:32 +0700

    chore: setup angular libs and core/shared/data-access

---
+++ apps/web/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'set-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web';
+}
---
+++ libs/core/shared/data-access/README.md
@@ -0,0 +1,7 @@
+# core-shared-data-access
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test core-shared-data-access` to execute the unit tests.
